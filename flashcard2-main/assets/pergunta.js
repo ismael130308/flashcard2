be110629b6844f8519af4a628296f3c6c532627f
ismael130308 <159185086+ismael130308@users.noreply.github.com>
2024-10-08 15:58:37 -0300

    1303

---
+++ flashcard2-main/assets/pergunta.js
@@ -0,0 +1,17 @@
+criarCartao(
+    'Programaçao',
+    'O que é python',
+    'O Python e uma linguagem de programação',
+)
+
+CriarCartao(
+    'Geografia',
+    'Qual é a capital da França',
+    'A capital da França é paris',
+)
+
+CriarCartao(
+    'Programação',
+    'O que é função',
+    'Uma função e um bloco de codigó que esecuta uma tarefa',
+)
