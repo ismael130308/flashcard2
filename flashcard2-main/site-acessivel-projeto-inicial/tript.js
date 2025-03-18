9cb32843e6596106c1fc9289c2910539ee729a6e
ismael130308 <159185086+ismael130308@users.noreply.github.com>
2025-03-18 11:17:35 -0300

    18/03/2025

---
+++ flashcard2-main/site-acessivel-projeto-inicial/tript.js
@@ -0,0 +1,10 @@
+document.addEventListener ('DOMContentLoaded',function(){
+
+    const aumentafontebotao = document.getElementById('aumentar-fonte')
+    const diminuifontebotao = document.getElementById('diminue-fonte')
+    let tamanhoAtuafonte
+}
+
+
+ 
+)
